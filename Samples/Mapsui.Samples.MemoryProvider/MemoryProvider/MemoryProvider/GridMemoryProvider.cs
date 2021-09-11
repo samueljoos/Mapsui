77733ab6f66af2c866338680be8769503f4c438c
Bert Temme <btemme@hotmail.com>
2021-09-11 10:37:52 +0200

    add memoryprovider sample

---
+++ Samples/Mapsui.Samples.MemoryProvider/MemoryProvider/MemoryProvider/GridMemoryProvider.cs
@@ -0,0 +1,67 @@
+﻿using Mapsui.Geometries;
+using Mapsui.Providers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Tiles.Tools;
+
+namespace MemoryProvider
+{
+    public class GridMemoryProvider : IProvider
+    {
+        private readonly double _maxVisible;
+        private static int _counter;
+
+        public GridMemoryProvider(double maxVisible)
+        {
+            this._maxVisible = maxVisible;
+        }
+
+        public string CRS { get; set; }
+
+        public BoundingBox GetExtents()
+        {
+            return new BoundingBox(-180, - 90, 180, 90);
+        }
+
+        public IEnumerable<IFeature> GetFeaturesInView(BoundingBox box, double resolution)
+        {
+            if (!double.IsNaN(box.Top) && resolution < _maxVisible )
+            {
+                Debug.WriteLine($"GetFeaturesInView called ({_counter++}): " + box + ": " + resolution);
+                var polygons = new List<Polygon>();
+                var tiles = Tilebelt.GetTilesOnLevel(new [] { box.MinX, box.MinY, box.MaxX, box.MaxY}, 14);
+                foreach(var t in tiles)
+                {
+                    var polygon = GetPolygon(t); 
+                    polygons.Add(polygon);
+                }
+
+                var selected = polygons.Select(g => new Feature { Geometry = g }).ToList();
+                return selected;
+
+            }
+            return null;
+        }
+
+
+        public Polygon GetPolygon(Tile tile)
+        {
+            var p = new Polygon();
+            p.ExteriorRing.Vertices.Add(ToMapsui(tile.BoundsLL()));
+            p.ExteriorRing.Vertices.Add(ToMapsui(tile.BoundsUL()));
+            p.ExteriorRing.Vertices.Add(ToMapsui(tile.BoundsUR()));
+            p.ExteriorRing.Vertices.Add(ToMapsui(tile.BoundsLR()));
+            p.ExteriorRing.Vertices.Add(ToMapsui(tile.BoundsLL()));
+            return p;
+        }
+
+        public static Point ToMapsui(Point2 point)
+        {
+            var p = new Point(point.X, point.Y);
+            return p;
+        }
+
+
+    }
+}
---
+++ Samples/Mapsui.Samples.MemoryProvider/MemoryProvider/MemoryProvider/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Mapsui;
+using Mapsui.Layers;
+using Mapsui.Projection;
+using Mapsui.Styles;
+using Mapsui.Utilities;
+using System;
+using Xamarin.Forms;
+using Color = Mapsui.Styles.Color;
+
+namespace MemoryProvider
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            var map = new Map
+            {
+                CRS = "EPSG:3857",
+                Transformation = new MinimalTransformation()
+            };
+
+            var tileLayer = OpenStreetMap.CreateTileLayer();
+
+            map.Layers.Add(tileLayer);
+            map.Layers.Add(CreateGridLayer("Grid", new Color(150, 150, 30, 64), new Color(252, 76, 2, 128), 200));
+
+            map.Widgets.Add(new Mapsui.Widgets.ScaleBar.ScaleBarWidget(map) { TextAlignment = Mapsui.Widgets.Alignment.Center, HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Left, VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Bottom });
+            map.RotationLock = true;
+            mapView.UseFling = false;
+            mapView.Map = map;
+            mapView.MyLocationEnabled = false;
+            var transformation = new MinimalTransformation();
+            var p1 = transformation.Transform("EPSG:4326", "EPSG:3857", new Mapsui.Geometries.Point(5, 50));
+
+            mapView.Navigator.CenterOn((Mapsui.Geometries.Point)p1);
+            mapView.Navigator.ZoomTo(35);
+        }
+
+
+        public ILayer CreateGridLayer(string layerName, Color fillColor, Color outlineColor, double maxVisible)
+        {
+            return new Layer(layerName)
+            {
+                DataSource = new GridMemoryProvider(maxVisible) { CRS = "EPSG:4326" },
+                Style = new VectorStyle
+                {
+                    Fill = new Mapsui.Styles.Brush(fillColor),
+                    Outline = new Pen
+                    {
+                        Color = outlineColor,
+                        Width = 4,
+                        PenStyle = PenStyle.Solid,
+                        PenStrokeCap = PenStrokeCap.Round
+                    }
+                },
+                MaxVisible = maxVisible
+            };
+        }
+    }
+}
